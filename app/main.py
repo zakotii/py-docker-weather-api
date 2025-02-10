860b1e777e0a5365c5a11b295f64e5e48c307b50
zakotii <abc1234567892777@gmail.com>
2025-02-10 21:50:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        print("Error: API_KEY is missing!")
+        return
+
+    url = f"http: //api.weatherapi.com/v1/current.json?key={api_key}&q=Paris"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Проверка на ошибки HTTP
+        data = response.json()
+        weather = data["current"]
+        print(
+            f"Paris, {data["location"]["country"]} - "
+            f"{data["location"]["localtime"]}")
+        print(
+            f"Weather: {weather["temp_c"]}°C, {weather["condition"]["text"]}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
 
 
 if __name__ == "__main__":

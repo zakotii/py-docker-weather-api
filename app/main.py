09d9b5725a10859535eb16d7698176507e7dea69
zakotii <abc1234567892777@gmail.com>
2025-02-11 20:23:58 +0200

    Solution5

--- app/main.py
+++ app/main.py
@@ -8,21 +8,22 @@ def get_weather() -> None:
         print("Error: API_KEY is missing!")
         return
 
-    url = f"http: //api.weatherapi.com/v1/current.json?key={api_key}&q=Paris"
+    url = "http://api.weatherapi.com/v1/current.json"
+    params = {"key": api_key, "q": "Paris"}
 
     try:
-        response = requests.get(url)
-        response.raise_for_status()  # Проверка на ошибки HTTP
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Перевірка на HTTP-помилки
         data = response.json()
         weather = data["current"]
 
         print(
-            f"Paris, {data["location"]["country"]} - "
-            f"{data["location"]["localtime"]}"
+            f'Paris, {data["location"]["country"]} - '
+            f'{data["location"]["localtime"]}'
         )
         print(
-            f"Weather: {weather["temp_c"]}°C, "
-            f"{weather["condition"]["text"]}"
+            f'Weather: {weather["temp_c"]}°C, '
+            f'{weather["condition"]["text"]}'
         )
     except requests.exceptions.RequestException as e:
         print(f"Error fetching weather data: {e}")
